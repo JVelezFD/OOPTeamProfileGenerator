41d2fa5ab2f0d8a6f3e707cb6423546782b65d10
JVelezFD <jvelez117@gmail.com>
2022-08-24 20:29:13 -0400

    added the babel env for jest to run with scripts created

--- test/manager.test.js
+++ test/manager.test.js
@@ -5,7 +5,7 @@ import Manager from "../lib/manager.js";
 const manager = new Manager('Jesus', 17, 'test@gmail.com', 5555555555);
 
 test('Office Number of manager', () => {
-    expect(manager.getOfficeNumber()).toEqual(55)
+    expect(manager.getOffNumber()).toEqual(5555555555)
 });
 
 test('Role of employee', () => {
